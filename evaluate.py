a75a27bffd94f9b13a3fd5d6d467b9120fed422e
TheBv <thebv939@gmail.com>
2022-03-11 13:12:21 +0100

    Fixed evaluation issues

--- evaluate.py
+++ evaluate.py
@@ -19,8 +19,8 @@ args = parser.parse_args()
 with open(args.relations) as file:
     data = file.read()
     pdata = pd.read_csv(StringIO(data), delimiter=',').values
-    X_train_valid, X_test = train_test_split_no_unseen(pdata, test_size=2)
-    X_train, X_valid = train_test_split_no_unseen(pdata, test_size=2)
+    X_train_valid, X_train = train_test_split_no_unseen(pdata, test_size=0.7)
+    X_test, X_valid = train_test_split_no_unseen(X_train_valid, test_size=0.5, allow_duplication=True)
     X = {'train': X_train,
          'valid': X_valid,
          'test': X_test}
@@ -32,24 +32,24 @@ filter_triples = np.concatenate((X['train'], X['valid'], X['test']))
 model = ComplEx(batches_count=10, seed=0, epochs=100, k=150, eta=1,
                 loss='nll', optimizer='adam')
 
-model.fit(X['train'], True, {
-    'x_valid': X['valid'][::2],
-    'criteria': 'mrr',
-    'x_filter': filter_triples,
-    'corruption_entities': pdata,
-    'stop_interval': 4,
-    'burn_in': 0,
-    'check_interval': 50
-})
+model.fit(np.concatenate((X['train'], X['valid'])))#, True, {
+#     'x_valid': X['valid'][::2],
+#     'criteria': 'mrr',
+#     'x_filter': filter_triples,
+#     'corruption_entities': pdata,
+#     'stop_interval': 4,
+#     'burn_in': 0,
+#     'check_interval': 50
+# })
 
 if not hasattr(model, 'early_stopping_epoch') or model.early_stopping_epoch is None:
     early_stopping_epoch = np.nan
 else:
     early_stopping_epoch = model.early_stopping_epoch
 
-ranks = evaluate_performance(X['test'], model=model,
+ranks = evaluate_performance(X['test'][:2000], model=model,
                              filter_triples=filter_triples,
-                             filter_unseen=False,
+                             filter_unseen=True,
                              entities_subset=pdata,
                              use_default_protocol=True)
 mr = mr_score(ranks)
@@ -66,7 +66,19 @@ print({
     "H@10": hits_10,
     "early_stopping_epoch": early_stopping_epoch
 })
+# 2000
+# no-bert {'mr': 3226.07225, 'mrr': 0.34979188900801544, 'H@1': 0.30075, 'H@3': 0.36475, 'H@10': 0.4445, 'early_stopping_epoch': nan}
+# all {'mr': 9135.432, 'mrr': 0.028332911786352732, 'H@1': 0.01825, 'H@3': 0.03125, 'H@10': 0.0445, 'early_stopping_epoch': nan}
+# 200
+# bert: {'mr': 2096.1925, 'mrr': 0.22525401640741383, 'H@1': 0.175, 'H@3': 0.2425, 'H@10': 0.325, 'early_stopping_epoch': nan}
+# no-bert: {'mr': 3491.7875, 'mrr': 0.34094036252146315, 'H@1': 0.29, 'H@3': 0.365,
+# 'H@10': 0.4375, 'early_stopping_epoch': nan}
+# all: {'mr': 9273.0025, 'mrr': 0.026636442633348956, 'H@1': 0.0175, 'H@3': 0.0325, 'H@10': 0.035, 'early_stopping_epoch': nan}
 
+# bert: {'mr': 1752.7, 'mrr': 0.16269749455382665, 'H@1': 0.1, 'H@3': 0.2, 'H@10': 0.2, 'early_stopping_epoch': nan}
+# all: {'mr': 8457.0, 'mrr': 0.10195046512959845, 'H@1': 0.1, 'H@3': 0.1, 'H@10': 0.1,
+# no_bert: {'mr': 5610.5, 'mrr': 0.24547178535937242, 'H@1': 0.2, 'H@3': 0.2, 'H@10': 0.4, 'early_stopping_epoch': nan}
+# 'early_stopping_epoch': nan}
 # (batches_count=10, seed=0, epochs=100, k=150, eta=1,
 #                 loss='nll', optimizer='adam')
 # only bert
